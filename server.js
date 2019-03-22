4c328395f0f2ad1c322f5bb5b57baceb32d9e0aa
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-03-21 18:22:28 -0700

    did git init, made functional server connection

---
+++ server.js
@@ -0,0 +1,22 @@
+//npm package
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+//sets the port
+var PORT = process.env.PORT || 8080;
+
+//create app/json parser
+var jsonParser = bodyParser.json();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+app.use(bodyParser.json({type: 'application/*+json'}))
+app.use(bodyParser.json({type: 'application/vnd.custom-type'}))
+app.use(bodyParser.json({type: 'text/html'}))
+
+//listener--effectively starting server
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
