ede4f372d97b6243c6112d005f52255ed667fd87
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-03-24 17:55:34 -0700

    getting closer--have alert popping up when button is clicked

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,140 +1,131 @@
-var friends = [
-{
-    name: "Arya Stark",
-    photo: "https://www.unhcr.org/mt/wp-content/uploads/sites/54/2018/03/Arya_stark_s7.jpg",
-    scores : 
-    [ 
-    "1",
-    "5",
-    "1",
-    "4",
-    "3",
-    "4",
-    "3",
-    "4",
-    "1",
-    "5",
-    ]
-},
-{
-    name: "Mia Wallace",
-    photo: "https://www.interviewmagazine.com/wp-content/uploads/2018/01/Mia-Wallace-Pulp-Fiction.jpg",
-    scores : 
-    [ 
-    "5",
-    "5",
-    "1",
-    "1",
-    "5",
-    "1",
-    "3",
-    "1",
-    "2",
-    "5",
-    ]
-},
-{
-    name: "Hermione Granger",
-    photo: "https://vignette.wikia.nocookie.net/harrypotter/images/4/41/Hermionedhface.jpg/revision/latest?cb=20161221044857",
-    scores : 
-    [ 
-    "5",
-    "1",
-    "5",
-    "5",
-    "5",
-    "5",
-    "1",
-    "5",
-    "5",
-    "5",
-    ]
-},
-{
-    name: "Katniss Everdeen",
-    photo: "https://vignette.wikia.nocookie.net/rifftrax/images/d/d8/Katniss-everdeen-gallery.jpg/revision/latest?cb=20140625224702",
-    scores : 
-    [ 
-    "1",
-    "5",
-    "1",
-    "5",
-    "1",
-    "3",
-    "5",
-    "5",
-    "2",
-    "3",
-    ]
-},
-{
-    name: "Clarice Starling",
-    photo: "https://en.wikipedia.org/wiki/Clarice_Starling#/media/File:SOTLClariceLecter.jpg",
-    scores : 
-    [ 
-    "1",
-    "1",
-    "2",
-    "4",
-    "5",
-    "3",
-    "3",
-    "3",
-    "3",
-    "1",
-    ]
-},
-{
-    name: "Jon Snow",
-    photo: "https://pbs.twimg.com/profile_images/901947348699545601/hqRMHITj_400x400.jpg",
-    scores : 
-    [ 
-    "2",
-    "3",
-    "1",
-    "3",
-    "1",
-    "5",
-    "3",
-    "1",
-    "2",
-    "3",
-    ]
-},
-{
-    name: "Voldemort",
-    photo: "https://en.wikipedia.org/wiki/Lord_Voldemort#/media/File:Lordvoldemort.jpg",
-    scores : 
-    [ 
-    "1",
-    "1",
-    "1",
-    "1",
-    "1",
-    "3",
-    "5",
-    "1",
-    "5",
-    "5",
-    ]
-},
-{
-    name: "Hannibal Lecter",
-    photo: "https://en.wikipedia.org/wiki/Hannibal_Lecter",
-    scores : 
-    [ 
-    "1",
-    "5",
-    "3",
-    "1",
-    "5",
-    "3",
-    "5",
-    "2",
-    "3",
-    "1",
-    ]
-}
+var friends = [{
+        name: "Arya Stark",
+        photo: "https://www.unhcr.org/mt/wp-content/uploads/sites/54/2018/03/Arya_stark_s7.jpg",
+        scores: [
+            "1",
+            "5",
+            "1",
+            "4",
+            "3",
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+        ]
+    },
+    {
+        name: "Mia Wallace",
+        photo: "https://www.interviewmagazine.com/wp-content/uploads/2018/01/Mia-Wallace-Pulp-Fiction.jpg",
+        scores: [
+            "5",
+            "5",
+            "1",
+            "1",
+            "5",
+            "1",
+            "3",
+            "1",
+            "2",
+            "5",
+        ]
+    },
+    {
+        name: "Hermione Granger",
+        photo: "https://vignette.wikia.nocookie.net/harrypotter/images/4/41/Hermionedhface.jpg/revision/latest?cb=20161221044857",
+        scores: [
+            "5",
+            "1",
+            "5",
+            "5",
+            "5",
+            "5",
+            "1",
+            "5",
+            "5",
+            "5",
+        ]
+    },
+    {
+        name: "Katniss Everdeen",
+        photo: "https://vignette.wikia.nocookie.net/rifftrax/images/d/d8/Katniss-everdeen-gallery.jpg/revision/latest?cb=20140625224702",
+        scores: [
+            "1",
+            "5",
+            "1",
+            "5",
+            "1",
+            "3",
+            "5",
+            "5",
+            "2",
+            "3",
+        ]
+    },
+    {
+        name: "Clarice Starling",
+        photo: "https://en.wikipedia.org/wiki/Clarice_Starling#/media/File:SOTLClariceLecter.jpg",
+        scores: [
+            "1",
+            "1",
+            "2",
+            "4",
+            "5",
+            "3",
+            "3",
+            "3",
+            "3",
+            "1",
+        ]
+    },
+    {
+        name: "Jon Snow",
+        photo: "https://pbs.twimg.com/profile_images/901947348699545601/hqRMHITj_400x400.jpg",
+        scores: [
+            "2",
+            "3",
+            "1",
+            "3",
+            "1",
+            "5",
+            "3",
+            "1",
+            "2",
+            "3",
+        ]
+    },
+    {
+        name: "Voldemort",
+        photo: "https://en.wikipedia.org/wiki/Lord_Voldemort#/media/File:Lordvoldemort.jpg",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "3",
+            "5",
+            "1",
+            "5",
+            "5",
+        ]
+    },
+    {
+        name: "Hannibal Lecter",
+        photo: "https://en.wikipedia.org/wiki/Hannibal_Lecter",
+        scores: [
+            "1",
+            "5",
+            "3",
+            "1",
+            "5",
+            "3",
+            "5",
+            "2",
+            "3",
+            "1",
+        ]
+    }
 ];
 
-module.exports = friends; 
+module.exports = friends;
--- server.js
+++ server.js
@@ -6,20 +6,25 @@ var bodyParser = require('body-parser');
 //sets the port
 var PORT = process.env.PORT || 8080;
 
-//create app/json parser
-var jsonParser = bodyParser.json();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false})
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.use(bodyParser.json({type: 'application/*+json'}))
-app.use(bodyParser.json({type: 'application/vnd.custom-type'}))
-app.use(bodyParser.json({type: 'text/html'}))
+app.use(bodyParser.json({
+  type: 'application/*+json'
+}))
+app.use(bodyParser.json({
+  type: 'application/vnd.custom-type'
+}))
+app.use(bodyParser.json({
+  type: 'text/html'
+}))
 
 require("./app/routing/htmlRoutes.js")(app);
 require("./app/routing/apiRoutes.js")(app);
 
 //listener--effectively starting server
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
-  
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
